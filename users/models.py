7cc90a38421fda10f1a9098fd735511d67499362
NohaAboutaleb <nohamesllhy133@gmail.com>
2024-04-04 11:51:26 +0200

    edit abstract user

--- users/models.py
+++ users/models.py
@@ -41,7 +41,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
+
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
