71286d6f5ee065bfe7c22d0b53aeb69c95e88b36
NohaAboutaleb <nohamesllhy133@gmail.com>
2024-04-07 12:04:11 +0200

    order status default value

--- shop/models.py
+++ shop/models.py
@@ -79,7 +79,7 @@ class Order(models.Model):
         ("PAID", "Paid"),
     ]
     user = models.ForeignKey(User, related_name="user_order", on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PAID")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PENDING")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     address = models.CharField(max_length=200)
